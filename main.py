b079a8d5dc10ee53bc5d3e20d1aa00483a4067b7
justctrlaltdel <102332459+justctrlaltdel@users.noreply.github.com>
2022-05-18 22:18:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+age = 31
+
+weight = 115.4 
+
+dress = 'pink' 
+
+store = 'TKaye\'s Pizza Shop, the "best" there is and ever will be'
+
+print (store)
+
+movie = 'BAPS - Living large, and taking charge baby baby!' 
+
+print (movie)
